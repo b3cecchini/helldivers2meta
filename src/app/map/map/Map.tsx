6a92ac3ec0bc24916d978ba4b706e8eb645a0cf7
b3cecchini <b3cecchini@gmail.com>
2024-02-26 01:08:25 -0500

    Map styling

--- src/app/map/map/Map.tsx
+++ src/app/map/map/Map.tsx
@@ -5,77 +5,33 @@ import {
   Polygon,
   useMapEvents,
   TileLayer,
+  Tooltip,
+  FeatureGroup,
+  Popup,
 } from "react-leaflet";
-import {
-  Divider,
-  HStack,
-  Image,
-  Text,
-  VStack,
-  Flex,
-  Heading,
-  Button,
-} from "@chakra-ui/react";
-
+import { Image, Text, VStack, Flex, Heading, Button } from "@chakra-ui/react";
+import { LuSwords } from "react-icons/lu";
 import { sectorData } from "../../../../public/data/sectorBounds";
 import L, { LatLng } from "leaflet";
 import Leaflet from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { useState } from "react";
 
-const geoJSON = {
-  type: "FeatureCollection",
-  features: [
-    {
-      type: "Feature",
-      geometry: { type: "Point", coordinates: [0, 0] },
-      properties: { prop0: "value0" },
-    },
-    {
-      type: "Feature",
-      geometry: {
-        type: "LineString",
-        coordinates: [
-          [22.0, 0.0],
-          [23.0, 1.0],
-          [24.0, 0.0],
-          [25.0, 1.0],
-        ],
-      },
-      properties: {
-        prop0: "value0",
-        prop1: 0.0,
-      },
-    },
-    // {
-    //   type: "Feature",
-    //   geometry: {
-    //     type: "Polygon",
-    //     coordinates: [
-    //       [
-    //         [100.0, 0.0],
-    //         [101.0, 0.0],
-    //         [101.0, 1.0],
-    //         [100.0, 1.0],
-    //         [100.0, 0.0],
-    //       ],
-    //     ],
-    //   },
-    //   properties: {
-    //     prop0: "value0",
-    //     prop1: { this: "that" },
-    //   },
-    // },
-  ],
+type PlanetData = {
+  name: string;
+  planetLiberatedPercent: number;
+  coordinates: number[];
+  defenseCampaign: boolean;
+  liberationCampaign: boolean;
 };
-
 type MarkerProps = {
-  position: number[];
+  planet: PlanetData;
   key: string;
+  children: any;
 };
 
-function LocationMarker({ position, key }: MarkerProps) {
-  const [iconSize, setIconSize] = useState<[number, number]>([15, 15]);
+function LocationMarker({ planet, key, children }: MarkerProps) {
+  const [iconSize, setIconSize] = useState<[number, number]>([5, 5]);
   const map = useMapEvents({
     zoomend() {
       switch (map.getZoom()) {
@@ -85,19 +41,19 @@ function LocationMarker({ position, key }: MarkerProps) {
           return;
         case 4:
           console.log("found 4");
-          setIconSize([40, 40]);
+          setIconSize([30, 30]);
           return;
         case 3:
           console.log("found 3");
-          setIconSize([30, 30]);
+          setIconSize([18, 18]);
           return;
         case 2:
           console.log("found 2");
-          setIconSize([20, 20]);
+          setIconSize([8, 8]);
           return;
         default:
           console.log("default");
-          setIconSize([15, 15]);
+          setIconSize([5, 5]);
           return;
       }
     },
@@ -108,14 +64,18 @@ function LocationMarker({ position, key }: MarkerProps) {
 
   return (
     <Marker
-      position={[position[0], position[1]]}
+      key={key}
+      position={[planet.coordinates[0], planet.coordinates[1]]}
       icon={
         new Leaflet.Icon({
           iconUrl: "/images/circle.png",
           iconSize: iconSize,
+          html: LuSwords,
         })
       }
-    />
+    >
+      {children}
+    </Marker>
   );
 }
 
@@ -140,35 +100,68 @@ export default function Map() {
 
         //zoomControl={() => {getIconSize()}}
       >
-        <TileLayer
+        {/* <TileLayer
           url="/images/map/{z}_{x}_{y}.jpg"
           noWrap={true}
           tileSize={256.23}
           zIndex={1}
-        />
+        /> */}
 
         {sectorData.map((sector, sectorIndex) => {
           return (
             <>
-              <Polygon
-                key={"polygon_shape_" + sectorIndex}
-                pathOptions={{
-                  color:
-                    sector.enemyType === "None"
-                      ? "skyblue"
-                      : sector.enemyType === "Terminids"
-                      ? "yellow"
-                      : "red",
-                }}
-                positions={sector.positions as unknown as LatLng[]}
-              />
+              <FeatureGroup>
+                <Popup> {sector.name}</Popup>
+                <Polygon
+                  key={"polygon_shape_" + sectorIndex}
+                  pathOptions={{
+                    color:
+                      sector.enemyType === "None"
+                        ? "skyblue"
+                        : sector.enemyType === "Terminids"
+                        ? "yellow"
+                        : "red",
+                  }}
+                  positions={sector.positions as unknown as LatLng[]}
+                />
+              </FeatureGroup>
               {sector.planets.map((planet, planetIndex) => {
-                if (planet.name === "Super Earth")
+                if (planet.coordinates[0] != 0)
                   return (
                     <LocationMarker
                       key={"sector_" + sectorIndex + "_planet_" + planetIndex}
-                      position={[planet.coordinates[0], planet.coordinates[1]]}
-                    />
+                      planet={planet}
+                    >
+                      {planet.defenseCampaign == true && <LuSwords />}
+                      <Tooltip
+                        opacity={1}
+                        direction="top"
+                        offset={[0, -10]}
+                        className="custom-leaflet-tooltip"
+                      >
+                        <VStack
+                          align="start"
+                          p={2}
+                          spacing={4}
+                          w={"100%"}
+                          bgColor={"rgba(0, 31, 63, 0.6)"}
+                        >
+                          <Heading
+                            as={"h2"}
+                            pr={8}
+                            color={"coal.50"}
+                            fontFamily="serif"
+                            fontSize="md"
+                            mb={5}
+                          >
+                            {planet.name}
+                          </Heading>
+                          <Text>
+                            {`${planet.planetLiberatedPercent}% Liberated`}
+                          </Text>
+                        </VStack>
+                      </Tooltip>
+                    </LocationMarker>
                   );
               })}
             </>
--- src/app/theme/theme.ts
+++ src/app/theme/theme.ts
@@ -11,6 +11,28 @@ export const theme = extendTheme({
       ".leaflet-container": {
         height: "100vh",
       },
+      ".custom-leaflet-tooltip": {
+        backgroundColor: "gba(0, 31, 63, 0.6)",
+        textColor: "blueviolet",
+      },
+      /**
+       * 
+       .leaflet-tooltip-own {
+          position: absolute;
+          padding: 4px;
+          background-color: rgba(0, 0, 0, 0.4);
+          border: 0px solid #000;
+          color: #000;
+          white-space: nowrap;
+          -webkit-user-select: none;
+          -moz-user-select: none;
+          -ms-user-select: none;
+          user-select: none;
+          pointer-events: none;
+          box-shadow: 0 1px 3px rgba(0,0,0,0.4);
+}
+          
+       */
     },
   },
 });
