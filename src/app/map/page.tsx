a1b262a5154b7295a8a87b18f49fd99e12aca332
Gregory Baumes <baumesgreg@gmail.com>
2024-02-26 17:11:11 -0500

    Added heading to warmap.

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,7 +1,48 @@
+import { Flex, Heading } from "@chakra-ui/react";
 import dynamic from "next/dynamic";
 
 const MapComponent = dynamic(() => import("./map/Map"), { ssr: false });
 
 export default async function PageMap() {
-  return <MapComponent />;
+  return (
+    <>
+      <Heading
+        mb={4}
+        textColor={"white"}
+        textShadow={`
+          -2px 2px 2px #000,
+				  2px 2px 2px #000,
+				  2px -2px 0 #000,
+				  -2px -2px 0 #000;`}
+      >
+        Galactic War Progress
+      </Heading>
+      <Flex justifyContent={"center"} gap={5} mb={2}>
+        <Heading
+          fontSize={24}
+          color={"red"}
+          textShadow={`
+          -2px 2px 2px #000,
+				  2px 2px 2px #000,
+				  2px -2px 0 #000,
+				  -2px -2px 0 #000;`}
+        >
+          Automatons
+        </Heading>
+        <Heading
+          fontSize={24}
+          color={"yellow"}
+          textShadow={`
+          -2px 2px 2px #000,
+				  2px 2px 2px #000,
+				  2px -2px 0 #000,
+				  -2px -2px 0 #000;`}
+        >
+          Terminids
+        </Heading>
+        \
+      </Flex>
+      <MapComponent />;
+    </>
+  );
 }
