eefdff5536853bc7a37a5190dc9875dd4e2f68a3
Gregory Baumes <baumesgreg@gmail.com>
2024-03-01 13:35:56 -0500

    Map update 3/1/24 1:35PM

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -64,7 +64,7 @@ export default async function PageMap() {
 				  -2px -2px 0 #000;`}
         mb={1}
       >
-        Last updated February 29, 2024 3:31PM EST
+        Last updated March 1, 2024 1:35PM EST
       </Text>
     </>
   );
